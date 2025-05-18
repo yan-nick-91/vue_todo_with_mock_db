c224a0ecd8aa05e87eea5192b1c3a69c64a1dee9
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-05-18 23:39:31 +0200

    Updated valueGenerator.
    ID can now be generate unique values now

--- src/util/valueGenerator.ts
+++ src/util/valueGenerator.ts
@@ -1,49 +1,49 @@
 const PAD_TWO_DIGITS = 2
-const pad = (numb: number, digits: number) => numb.toString().padStart(digits, '0')
-const now = new Date()
+const pad = (number: number, digits: number) => number.toString().padStart(digits, '0')
 
 export const generateTaskId = () => {
-  const dateStringValue = generateDateStringValue()
-  const timeStringValue = generatedTimeStringValue()
-
+  const now = new Date()
+  const dateStringValue = generateDateStringValue(now)
+  const timeStringValue = generatedTimeStringValue(now)
   return `${dateStringValue}-${timeStringValue}`
 }
 
 export const generateCurrentDate = () => {
-  const { day, month, year } = dateGenerator()
-  return `${day}-${month}-${year}, ${generateCurrentTimeValue()}`
+  const now = new Date()
+  const { day, month, year } = dateGenerator(now)
+  return `${day}-${month}-${year}, ${generateCurrentTimeValue(now)}`
 }
 
 export const generateBulletItemId = () => `bid-${Math.random().toString(36).substring(2, 10)}`
 
-const generateDateStringValue = () => {
-  const { day, month, year } = dateGenerator()
+const generateDateStringValue = (now: Date) => {
+  const { day, month, year } = dateGenerator(now)
   return `${day}${month}${year}`
 }
 
-const generateCurrentTimeValue = () => {
-  const currentHour = generateCurrentHour()
-  const currentMinute = generateCurrentMinute()
+const generateCurrentTimeValue = (now: Date) => {
+  const currentHour = generateCurrentHour(now)
+  const currentMinute = generateCurrentMinute(now)
 
   return `${currentHour}:${currentMinute}`
 }
 
-const generatedTimeStringValue = () => {
+const generatedTimeStringValue = (now: Date) => {
   const PAD_SIX_DIGITS = 6
-  const hours = generateCurrentHour()
-  const minutes = generateCurrentMinute()
-  const seconds = generateCurrentSeconds()
-  const milliseconds = generateCurrentMilliSeconds(PAD_SIX_DIGITS)
+  const hours = generateCurrentHour(now)
+  const minutes = generateCurrentMinute(now)
+  const seconds = generateCurrentSeconds(now)
+  const milliseconds = generateCurrentMilliSeconds(now, PAD_SIX_DIGITS)
 
   return `${hours}${minutes}${seconds}${milliseconds}`
 }
 
-const generateCurrentHour = () => pad(now.getHours(), PAD_TWO_DIGITS)
-const generateCurrentMinute = () => pad(now.getMinutes(), PAD_TWO_DIGITS)
-const generateCurrentSeconds = () => pad(now.getSeconds(), PAD_TWO_DIGITS)
-const generateCurrentMilliSeconds = (digits: number) => pad(now.getSeconds(), digits)
+const generateCurrentHour = (now: Date) => pad(now.getHours(), PAD_TWO_DIGITS)
+const generateCurrentMinute = (now: Date) => pad(now.getMinutes(), PAD_TWO_DIGITS)
+const generateCurrentSeconds = (now: Date) => pad(now.getSeconds(), PAD_TWO_DIGITS)
+const generateCurrentMilliSeconds = (now: Date, digits: number) => pad(now.getSeconds(), digits)
 
-const dateGenerator = () => {
+const dateGenerator = (now: Date) => {
   const day = pad(now.getDate(), PAD_TWO_DIGITS)
   const month = pad(now.getMonth() + 1, PAD_TWO_DIGITS)
   const year = now.getFullYear()
