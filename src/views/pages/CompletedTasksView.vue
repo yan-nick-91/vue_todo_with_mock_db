a7137d50718d2cac5ea5ad413c54e1e3738d9c2d
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-05-19 23:08:52 +0200

    Updated CompletedTaskView

--- src/views/pages/CompletedTasksView.vue
+++ src/views/pages/CompletedTasksView.vue
@@ -1,30 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { getAllFinishedTasks } from '@/controller/task-controller'
-import type { Task } from '@/interface/task'
-import TheTask from '../components/home/TheTask.vue'
-
-const finishedTasks = ref<Task[]>([])
-
-const getFinishedTasks = async () => {
-  try {
-    const tasks = await getAllFinishedTasks()
-    finishedTasks.value = tasks
-  } catch (error) {
-    console.error('Error fetching finished tasks:', error)
-  }
-}
-
-onMounted(() => {
-  getFinishedTasks()
-})
+import TheCompletedTaskComponent from '../components/task/TheCompletedTaskComponent.vue'
 </script>
 
 <template>
-  <h1>About</h1>
-  <ul>
-    <li v-for="task in finishedTasks" :key="task.id">
-      <TheTask :task="task" />
-    </li>
-  </ul>
+  <TheCompletedTaskComponent />
 </template>
