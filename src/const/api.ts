0382297a5227a2c4412e76fc27554fc64c07c0b0
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-05-26 12:44:28 +0200

    Changed hardcode value with .env declarations

--- src/const/api.ts
+++ src/const/api.ts
@@ -1,4 +1,4 @@
-const PORT = 3001
-const HOST = 'http://localhost:'
+const PORT = import.meta.env.VITE_PORT // Declare the value in the .env at the project root as VITE_PORT (e.g., VITE_PORT=3000)
+const HOST = import.meta.env.VITE_HOST // Declare the value in the .env at the project root as VITE_HOST (e.g., VITE_HOST=http://localhost)
 
-export const TASK_ENDPOINT = `${HOST}${PORT}/tasks`
+export const TASK_ENDPOINT = `${HOST}:${PORT}/tasks`
