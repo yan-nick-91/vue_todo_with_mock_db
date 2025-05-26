eaed2ae5f75b81224a721243d76b0454ec7cc49a
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-05-26 12:45:21 +0200

    Hotfix: changed 3000 to 3001

--- src/const/api.ts
+++ src/const/api.ts
@@ -1,4 +1,4 @@
-const PORT = import.meta.env.VITE_PORT // Declare the value in the .env at the project root as VITE_PORT (e.g., VITE_PORT=3000)
+const PORT = import.meta.env.VITE_PORT // Declare the value in the .env at the project root as VITE_PORT (e.g., VITE_PORT=3001)
 const HOST = import.meta.env.VITE_HOST // Declare the value in the .env at the project root as VITE_HOST (e.g., VITE_HOST=http://localhost)
 
 export const TASK_ENDPOINT = `${HOST}:${PORT}/tasks`
