b73dc21868615f1c14df39232c8e5849ded17040
Yannick-at-merlinq <yannick@merlinq.net>
2025-06-05 16:48:53 +0200

    exchanged browser to context

--- features/support/pageFixture.js
+++ features/support/pageFixture.js
@@ -2,5 +2,5 @@ export const pageFixture = {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   page: undefined, // as Page,
-  browser: undefined, // as Browser
+  context: undefined, // as BrowserContext
 }
