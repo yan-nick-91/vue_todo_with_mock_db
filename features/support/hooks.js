fe4a78836150aa9130b511e0c1dae85cc3aa5640
Yannick-at-merlinq <yannick@merlinq.net>
2025-06-05 16:48:53 +0200

    Init hook.js

---
+++ features/support/hooks.js
@@ -0,0 +1,72 @@
+import { After, Before, setDefaultTimeout, Status } from '@cucumber/cucumber'
+import { chromium, firefox, webkit } from '@playwright/test'
+import { pageFixture } from './pageFixture.js'
+
+console.log('Hooks file is loaded')
+
+const config = {
+  headless: false,
+  browser: 'chromium',
+  width: '1920',
+  height: '1080',
+}
+
+console.log(config)
+
+const browsers = {
+  chromium: chromium,
+  firefox: firefox,
+  webkit: webkit,
+}
+
+let browserInstance = null
+
+setDefaultTimeout(60 * 1000) // 1 min
+
+const initializeBrowserSetup = async (selectedBrowser) => {
+  const launchBrowser = browsers[selectedBrowser]
+
+  if (!launchBrowser) {
+    throw new Error(
+      `Selected Browser is invalid. Please choose on of the three options: chromium, firefox, webkit`,
+    )
+  }
+
+  return await launchBrowser.launch({ headless: config.headless })
+}
+
+Before(async function () {
+  try {
+    browserInstance = await initializeBrowserSetup(config.browser)
+    console.log(`Browser setup initialized for: ${config.browser}`)
+
+    const context = await browserInstance.newContext({
+      viewport: {width: parseInt(config.width), height: parseInt(config.height)}
+    })
+
+    const page = await context.newPage()
+    pageFixture.page = page
+  } catch (error) {
+    console.error('Something went wrong during the initialization process:', error)
+  }
+})
+
+After(async function ({ pickle, result }) {
+  if (result?.status === Status.FAILED) {
+    if (pageFixture.page) {
+      const screenshotPath = `./reports/screenshots/${pickle.name}-${Date.now()}.png`
+      const image = await pageFixture.page.screenshot({
+        path: screenshotPath,
+        type: 'png',
+      })
+      await this.attach(image, 'image/png')
+    } else {
+      console.error("pageFixture.page is undefined")
+    }
+  }
+
+  if (browserInstance) {
+    await pageFixture.page?.close()
+    await browserInstance.close()
+  }
+})
