95ef43a6d09fa5d6b12892a2fa7510ea52fc6cc1
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-06-14 19:35:03 +0200

    Init UI BaseCircleNotification

---
+++ src/views/UI/BaseCircleNotification.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  counterDisplay: {
+    type: [String, Number],
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="pr-1 flex items-center gap-2 cursor-pointer">
+    <span
+      v-if="counterDisplay"
+      class="flex h-5 w-5 items-center justify-center rounded-full px-2 py-1 text-xs font-bold text-white bg-red-600"
+    >
+      {{ counterDisplay }}
+    </span>
+  </div>
+</template>
