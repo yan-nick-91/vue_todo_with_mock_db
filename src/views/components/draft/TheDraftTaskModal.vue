6f51951752e7857422cd19ad00190d9f421f9787
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-06-14 19:37:28 +0200

    Init TheDraftTaskModal

---
+++ src/views/components/draft/TheDraftTaskModal.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import TaskForm from '../task/TaskForm.vue'
+import type { Task } from '@/interface/Task'
+
+defineProps({
+  modalIsOpen: {
+    type: Boolean,
+  },
+  draftedTasks: {
+    type: Object as PropType<Task>,
+  },
+})
+
+const emit = defineEmits(['open:draft', 'close:draft'])
+</script>
+
+<template>
+  <TaskForm
+    :mode="'draft'"
+    :modal-is-open="modalIsOpen"
+    :drafted-task="draftedTasks"
+    @close="emit('close:draft')"
+  />
+</template>
