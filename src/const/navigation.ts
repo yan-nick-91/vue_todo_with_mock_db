27daa3754bb4fa0e948c499b9cfaf6ec5d7350f3
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-06-14 19:26:39 +0200

    Added new link to navigation list

--- src/const/navigation.ts
+++ src/const/navigation.ts
@@ -1,4 +1,5 @@
 export const LINKS = [
   { id: 'p1', nameToPage: 'Home', url: '/' },
   { id: 'p2', nameToPage: 'Completed Task', url: '/completed-tasks' },
+  { id: 'p3', nameToPage: 'Drafts', url: '/drafts' },
 ]
