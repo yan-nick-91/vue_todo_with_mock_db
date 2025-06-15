65e9feb7cf6c88dcd917fe277247d3bbd55b60f0
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-06-15 02:22:11 +0200

    Updated UI Form for TheDraftTaskModal

--- src/views/components/draft/TheDraftTaskModal.vue
+++ src/views/components/draft/TheDraftTaskModal.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
-import TaskForm from '../task/TaskForm.vue'
 import type { Task } from '@/interface/Task'
+import TaskForm from '../task/TaskForm.vue'
 
 defineProps({
   modalIsOpen: {
@@ -16,10 +16,19 @@ const emit = defineEmits(['open:draft', 'close:draft'])
 </script>
 
 <template>
-  <TaskForm
-    :mode="'draft'"
-    :modal-is-open="modalIsOpen"
-    :drafted-task="draftedTasks"
-    @close="emit('close:draft')"
-  />
+  <div
+    v-if="modalIsOpen"
+    class="fixed inset-0 z-40 flex items-center justify-center bg-black/40"
+    @click="emit('close:draft')"
+  >
+    <div @click.stop>
+      <TaskForm
+        :mode="'draft'"
+        :modal-is-open="modalIsOpen"
+        :drafted-task="draftedTasks"
+        @close="emit('close:draft')"
+        class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white shadow-xl"
+      />
+    </div>
+  </div>
 </template>
