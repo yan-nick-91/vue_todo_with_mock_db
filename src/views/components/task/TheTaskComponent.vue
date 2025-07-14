65159d83b8217e4ebc70c6c04f582b984c1cdfe4
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-07-14 13:45:50 +0200

    Moved container 15rem from top

--- src/views/components/task/TheTaskComponent.vue
+++ src/views/components/task/TheTaskComponent.vue
@@ -166,77 +166,75 @@ const onTaskUpdated = (updatedTask: Task) => {
 </script>
 
 <template>
-  <section>
-    <BaseContainer class="mx-auto my-5 p-4" is-bordered>
-      <h2 class="text-2xl font-bold mb-4">
-        Task: {{ task.task }} {{ task.isFinished ? '(Finished)' : '' }}
-      </h2>
-      <em><strong>Created at:</strong> {{ task.createdAt }}</em>
-      <br />
-      <em v-if="task.updatedAt"><strong>Updated at:</strong> {{ task.updatedAt }}</em>
+  <BaseContainer class="mx-auto my-5 p-4 mt-15" is-bordered>
+    <h2 class="text-2xl font-bold mb-4">
+      Task: {{ task.task }} {{ task.isFinished ? '(Finished)' : '' }}
+    </h2>
+    <em><strong>Created at:</strong> {{ task.createdAt }}</em>
+    <br />
+    <em v-if="task.updatedAt"><strong>Updated at:</strong> {{ task.updatedAt }}</em>
 
-      <p class="my-4">
-        <strong>Task Details:</strong>
-      </p>
+    <p class="my-4">
+      <strong>Task Details:</strong>
+    </p>
 
-      <p class="my-4"><strong>Priority:</strong> {{ task.priority }}</p>
+    <p class="my-4"><strong>Priority:</strong> {{ task.priority }}</p>
 
-      <!-- Bullet List -->
-      <div class="mb-2">
-        <ul
-          role="list"
-          v-if="task.bulletList.length > BULLET_ITEM_LIST_IN_TASK_IS_EMPTY"
-          class="list-disc pl-6 space-y-2 text-gray-700"
-        >
-          <li v-for="(bullet, index) in task.bulletList" :key="index">
-            <div class="flex gap-2 items-start">
-              <span :class="{ 'line-through': bullet.itemIsFinished }" class="min-w-[15%]">
-                {{ bullet.bulletItem }}
-              </span>
-              <span>
-                <BaseButton
-                  v-show="!task.isFinished"
-                  class="p-1 rounded cursor-pointer transform active:scale-95"
-                  :btn-type="bullet.itemIsFinished ? DANGER : SUCCESS"
-                  @click="toggleCompletion(index)"
-                  >{{ bullet.itemIsFinished ? 'Undone' : 'Done' }}</BaseButton
-                >
-              </span>
-            </div>
-          </li>
-        </ul>
-        <BaseMessageDisplay v-else :message="'No details available'" />
-      </div>
+    <!-- Bullet List -->
+    <div class="mb-2">
+      <ul
+        role="list"
+        v-if="task.bulletList.length > BULLET_ITEM_LIST_IN_TASK_IS_EMPTY"
+        class="list-disc pl-6 space-y-2 text-gray-700"
+      >
+        <li v-for="(bullet, index) in task.bulletList" :key="index">
+          <div class="flex gap-2 items-start">
+            <span :class="{ 'line-through': bullet.itemIsFinished }" class="min-w-[15%]">
+              {{ bullet.bulletItem }}
+            </span>
+            <span>
+              <BaseButton
+                v-show="!task.isFinished"
+                class="p-1 rounded cursor-pointer transform active:scale-95"
+                :btn-type="bullet.itemIsFinished ? DANGER : SUCCESS"
+                @click="toggleCompletion(index)"
+                >{{ bullet.itemIsFinished ? 'Undone' : 'Done' }}</BaseButton
+              >
+            </span>
+          </div>
+        </li>
+      </ul>
+      <BaseMessageDisplay v-else :message="'No details available'" />
+    </div>
 
-      <hr />
-      <div class="flex gap-2 mt-2">
-        <BaseButton
-          v-show="!task.isFinished"
-          :btn-type="INFO"
-          class="cursor-pointer p-2 rounded transform active:scale-95"
-          @click="openUpdateModal"
-        >
-          Update
-        </BaseButton>
-        <BaseButton
-          :btn-type="DANGER"
-          class="cursor-pointer p-2 rounded transform active:scale-95"
-          @click="openDeleteModal"
-        >
-          Delete
-        </BaseButton>
-        <BaseButton
-          @click="taskIsCompleted"
-          :btn-type="SUCCESS"
-          class="p-2 rounded cursor-pointer transform active:scale-95"
-          >{{ task.isFinished ? 'Mark as Unfinished' : 'Finish Task' }}</BaseButton
-        >
-      </div>
-      <p v-if="errorMessage" class="text-red-500 mt-2">
-        {{ errorMessage }}
-      </p>
-    </BaseContainer>
-  </section>
+    <hr />
+    <div class="flex gap-2 mt-2">
+      <BaseButton
+        v-show="!task.isFinished"
+        :btn-type="INFO"
+        class="cursor-pointer p-2 rounded transform active:scale-95"
+        @click="openUpdateModal"
+      >
+        Update
+      </BaseButton>
+      <BaseButton
+        :btn-type="DANGER"
+        class="cursor-pointer p-2 rounded transform active:scale-95"
+        @click="openDeleteModal"
+      >
+        Delete
+      </BaseButton>
+      <BaseButton
+        @click="taskIsCompleted"
+        :btn-type="SUCCESS"
+        class="p-2 rounded cursor-pointer transform active:scale-95"
+        >{{ task.isFinished ? 'Mark as Unfinished' : 'Finish Task' }}</BaseButton
+      >
+    </div>
+    <p v-if="errorMessage" class="text-red-500 mt-2">
+      {{ errorMessage }}
+    </p>
+  </BaseContainer>
   <DeleteTaskModal
     v-if="deleteBtnSelected"
     class="mx-auto"
