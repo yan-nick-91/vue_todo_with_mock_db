55bfb277087ccc30717e836aaf21eae4e3348147
Yannick-at-merlinq <yannick@merlinq.net>
2025-07-30 17:35:52 +0200

    Modified store for global filtering across components

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -4,6 +4,8 @@ import { defineStore } from 'pinia'
 import { getAllDraftedTasks, getAllFinishedTasks, getTasks } from '@/controller/task-controller'
 import { computed, ref } from 'vue'
 import type { Task } from '@/interface/Task'
+import type { PRIORITIES } from '@/const/base-types'
+import { FilterTaskMode } from '@/const/enums/ModeStates'
 
 export const taskStore = defineStore('taskStore', () => {
   const allTasks = ref<Task[]>([])
@@ -12,8 +14,12 @@ export const taskStore = defineStore('taskStore', () => {
   const finishedTasks = ref<Task[]>([])
   const selectedTask = ref<Task | null>(null)
 
-  const refreshTasks = async () => {
+  // const filteredTaskMode = ref<FilterTaskMode>()
+  const filteredTasks = ref<Task[]>([])
+  const selectedPriority = ref<(typeof PRIORITIES)[number] | ''>('')
+  const filterApplied = ref(false)
 
+  const refreshTasks = async () => {
     createdTasks.value = await getTasks()
     draftedTasks.value = await getAllDraftedTasks()
     finishedTasks.value = await getAllFinishedTasks()
@@ -23,6 +29,12 @@ export const taskStore = defineStore('taskStore', () => {
     createdTasks.value.filter((task) => !task.isDrafted && !task.isFinished),
   )
 
+  const completedTasks = computed(() => {
+    const tasks = finishedTasks.value.filter((task) => !task.isDrafted && task.isFinished)
+    if (!filterApplied.value || !selectedPriority.value) return tasks
+    return tasks.filter((task) => task.priority === selectedPriority.value)
+  })
+
   const fetchTaskById = async (id: string) => {
     try {
       selectedTask.value = await getTaskId(id)
@@ -31,13 +43,50 @@ export const taskStore = defineStore('taskStore', () => {
     }
   }
 
+  const filterByPriority = (taskMode: FilterTaskMode) => {
+    const baseTasks = getTaskByMode(taskMode)
+
+    if (!selectedPriority.value) {
+      filteredTasks.value = baseTasks
+      filterApplied.value = false
+    } else {
+      filteredTasks.value = baseTasks.filter((task) => task.priority === selectedPriority.value)
+      filterApplied.value = true
+    }
+  }
+
+  const getTaskByMode = (taskMode: FilterTaskMode) => {
+    switch (taskMode) {
+      case FilterTaskMode.HOME:
+        return activeTasks.value
+      case FilterTaskMode.COMPLETED:
+        return completedTasks.value
+      default:
+        return []
+    }
+  }
+
+  const clearAllFiltering = (taskMode: FilterTaskMode) => {
+    selectedPriority.value = ''
+    filteredTasks.value = getTaskByMode(taskMode)
+    filterApplied.value = false
+  }
+
   return {
+    // state
     allTasks,
     activeTasks,
+    completedTasks,
     draftedTasks,
     finishedTasks,
     selectedTask,
+    selectedPriority,
+    filterApplied,
+    filteredTasks,
+    // actions
     refreshTasks,
     fetchTaskById,
+    filterByPriority,
+    clearAllFiltering,
   }
 })
