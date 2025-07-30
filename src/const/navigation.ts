7c00e215e89b13a1724efd37b828e66a217406ba
Yannick-at-merlinq <yannick@merlinq.net>
2025-07-30 15:40:15 +0200

    Fix typo in nav

--- src/const/navigation.ts
+++ src/const/navigation.ts
@@ -1,5 +1,5 @@
 export const LINKS = [
   { id: 'p1', nameToPage: 'Home', url: '/' },
-  { id: 'p2', nameToPage: 'Completed Task', url: '/completed-tasks' },
+  { id: 'p2', nameToPage: 'Completed Tasks', url: '/completed-tasks' },
   { id: 'p3', nameToPage: 'Drafts', url: '/drafts' },
 ]
